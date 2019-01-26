09ff6e4338b8b45e49012fb37d4b8b132820166e
Daniel Ritter <danieldritter1@gmail.com>
2019-01-26 13:31:54 -0600

    Added function to convert directory of images into training data

--- img_convert.py
+++ img_convert.py
@@ -1,5 +1,8 @@
 import numpy as np
 from PIL import Image
+import glob
+import os
+
 
 def convert_image(jpg):
 	'''
@@ -11,7 +14,6 @@ def convert_image(jpg):
 	Returns:
 	a list representing black and white pixel values
 	'''
-
 	im = Image.open(jpg)
 	bw = im.convert("L")
 
@@ -21,6 +23,27 @@ def normalize_image(image):
 	'''
 	Method to normalize a bw pixel values 
 	'''
-
 	image = image / 255
 	return image
+
+
+def convert_directory(directory_name):
+	'''
+	Method to convert a directory of images into a list of lists of black and white pixel values 
+
+	Params: pathname of the directory
+
+	Output: A list of lists of integers 
+	'''
+
+	output_list = []
+
+	for picture in os.listdir(directory_name):
+		output_list.append(convert_image(directory_name+"/"+picture))
+
+	return output_list
+
+
+
+
+
