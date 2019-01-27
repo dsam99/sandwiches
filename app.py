57c33a112f0fc4dab445a60c75a15a0b74b8b38a
juliamcclellan <julia.mcclellan@tamr.com>
2019-01-27 01:47:10 -0500

    commenting out model

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import base64
 app = Flask(__name__, template_folder="templates")
 
 MODEL_FILENAME = "sandwich_model_dropout2.h5"
-model = load_model(MODEL_FILENAME)
+#model = load_model(MODEL_FILENAME)
 
 # Create a URL route in our application for "/"
 @app.route('/')
@@ -39,7 +39,7 @@ def classify():
     convert_and_save(b64_string, "jpg")#base64.b64decode(b64_string), "jpg")
     # process it
     img_filename = "tmp/imageToSave.jpg"
-    cube_type = predict_class(model, img_filename)
+    #cube_type = predict_class(model, img_filename)
     #cube_type = 1
     # delete the image
     d = {
