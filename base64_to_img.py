818444060e151373978470094f128f63a1d036cc
Tyler Jiang <tylerj81@gmail.com>
2019-01-27 00:33:02 -0500

    im going to sleep

--- base64_to_img.py
+++ base64_to_img.py
@@ -2,7 +2,7 @@ import base64
 
 def convert_and_save(b64_string, ext="png"):
     with open("tmp/imageToSave." + ext, "wb") as fh:
-        fh.write(base64.decodebytes(b64_string))
+        fh.write(base64.decode(b64_string))
 
 
 def img_to_base_64(img_filename):
@@ -10,7 +10,7 @@ def img_to_base_64(img_filename):
         encoded_string = base64.b64encode(image_file.read())
         return encoded_string
 
-# base64_string = img_to_base_64('twitter.jpeg')
-# print(base64.b64encode(base64_string))
+base64_string = img_to_base_64('download.png')
+print(base64.b64encode(base64_string))
 
 # convert_and_save(base64_string, 'jpeg')
